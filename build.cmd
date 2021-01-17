26730780672cfa21446fab5acc1f7da533798d5a
glopesdev <glopesdev@users.noreply.github.com>
2021-01-17 13:33:00 +0000

    Update build scale

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 for %%I in (*.tex) do (
   latex -output-directory=build %%I
-  dvisvgm --no-fonts --scale=2.142857 --output=build/%%f.svg build/%%~nI.dvi
+  dvisvgm --no-fonts --scale=2.13 --output=build/%%f.svg build/%%~nI.dvi
 )
 del /S "*.dvi" "*.aux" "*.log"
