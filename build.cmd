ae651ff95e859ff5aa120ad18f70869fd3c91f5e
glopesdev <glopesdev@users.noreply.github.com>
2024-09-16 15:00:52 +0100

    Add docfx article export

--- build.cmd
+++ build.cmd
@@ -1,6 +1,14 @@
 @echo off
 for %%I in (*.tex) do (
-  latex -output-directory=build %%I
-  dvisvgm --no-fonts --scale=2.13 --output=build/%%f.svg build/%%~nI.dvi
+  if not exist "build\reactive-%%~nI.svg" (
+    echo --->build/reactive-%%~nI.md
+    echo uid: reactive-%%~nI>>build/reactive-%%~nI.md
+    echo title: %%~nI>>build/reactive-%%~nI.md
+    echo --->>build/reactive-%%~nI.md
+    echo(>>build/reactive-%%~nI.md
+    echo ^!^[Marble diagram^]^(~/images/reactive-%%~nI.svg^)>>build/reactive-%%~nI.md
+    latex -output-directory=build %%I
+    dvisvgm --no-fonts --scale=2.13 --output=build/reactive-%%f.svg build/%%~nI.dvi
+  )
 )
-del /S "*.dvi" "*.aux" "*.log"
+del /S "*.dvi" "*.aux" "*.log" "*.tex.md"
